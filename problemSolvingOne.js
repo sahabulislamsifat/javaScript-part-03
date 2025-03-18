77b54a26551dd5d8724fc8b172287bb548f7568d
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-03-19 04:01:14 +0600

    w3 schools 1 to 30 lessons problem solve

--- problemSolvingOne.js
+++ problemSolvingOne.js
@@ -34,7 +34,7 @@
 
 // console.log(roll);
 
-// Kono year leap year ki na ta ber korbo ki vabe???
+// 03. Kono year leap year ki na ta ber korbo ki vabe???
 
 // function isLeapYear(year) {
 //   if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
@@ -50,14 +50,50 @@
 // isLeapYear(2029);
 // isLeapYear(2030);
 
-const findLeapYear = (year) => {
-  if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
-    console.log(`${year} is a leap year...`);
-  } else {
-    console.log(`${year} is not a leap year...`);
-  }
-};
+// const findLeapYear = (year) => {
+//   if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
+//     console.log(`${year} is a leap year...`);
+//   } else {
+//     console.log(`${year} is not a leap year...`);
+//   }
+// };
 
 // findLeapYear(2026);
 // findLeapYear(2027);
-findLeapYear(2028);
+// findLeapYear(2028);
+
+// 04. kono sentence koto guli Vowel ache ta nirnoy kora jay ki vabe??
+
+// const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+
+// const countVowels = (sentence) => {
+//   let count = 0;
+//   const letters = Array.from(sentence);
+
+//   letters.forEach(function (value) {
+//     if (vowels.includes(value)) {
+//       count++;
+//     }
+//   });
+
+//   return count;
+// };
+
+// console.log(countVowels("I love Bangladesh...My hometown Rangpur."));
+
+// 05. kono array theke duplicate number guli ki vabe ber korboo???
+
+const numbers = [1, 6, 4, 6, 8, 4, 5, 6, 8, 10, 3, 7, 3];
+
+// const duplicates = numbers.filter(function (value, index, array) {
+//   return array.indexOf(value) !== index;
+// });
+
+// console.log(duplicates);
+
+// find unique numbers in the array
+const duplicates = numbers.filter(function (value, index, array) {
+  return array.indexOf(value) === index;
+});
+
+console.log(duplicates);
